faa83574fe42226daa4cdff964c84cdaa0e6de25
Oleg Shtanko <xiaklizrum@gmail.com>
2019-08-24 15:47:55 +1000

    Create if not exist for tables

--- dump.sql
+++ dump.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `sign_in_requests` (
+CREATE TABLE IF NOT EXISTS `sign_in_requests` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL DEFAULT '',
   `token` varchar(50) NOT NULL DEFAULT '',
@@ -7,7 +7,7 @@ CREATE TABLE `sign_in_requests` (
   `expired` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
