472c13e6a16acd3bb4781bd427a1d2b33e4b053b
Oleg Shtanko <xiaklizrum@gmail.com>
2021-05-17 00:13:16 +1000

    donenv for Mysql params

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+from dotenv.main import dotenv_values
 from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -5,9 +7,17 @@ from .creditals import *
 
 app = Flask(__name__)
 app.config.from_object('config')
+project_root = os.path.join(os.path.dirname(__file__), '../')
+config = dotenv_values(project_root + '.env') 
+
 engine = create_engine(
     'mysql://{}:{}@{}/{}'.format(
-        MYSQL_USER, MYSQL_PASSWORD, MYSQL_SERVER, MYSQL_DATABASE))
+        config['MYSQL_USER'],
+        config['MYSQL_PASSWORD'],
+        config['MYSQL_SERVER'],
+        config['MYSQL_DATABASE']
+    )
+)
 session = sessionmaker(bind=engine)
 session = session()
 from app import views
